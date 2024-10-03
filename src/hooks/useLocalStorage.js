a2196cc15dfd4c7ffcd56b4680eade9663107bc1
ArunRajput03 <arunrajput03@yahoo.com>
2024-10-03 18:32:15 +0000

    local storage hoks

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,8 +1,24 @@
-const useLocalStorage = (data, key) => {
-  const locData = localStorage.getItem(key)
-  if (locData === null) {
-    //set Data here
-  }
+import { useEffect, useState } from "react"
+
+const useLocalStorage = (key, initalData) => {
+  const [value, setValue] = useState(() => {
+    const locData = localStorage.getItem(key)
+    if (locData === null) {
+      return initalData
+    }
+
+    return JSON.parse(locData)
+  })
+
+  useEffect(() => {
+    if (value === undefined) {
+      localStorage.removeItem(key)
+    } else {
+      localStorage.setItem(key, JSON.stringify(value))
+    }
+  }, [value, key])
+
+  return [value, setValue]
 }
 
 export default useLocalStorage
